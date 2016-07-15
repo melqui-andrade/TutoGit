6b47ea3ac486654556ef270e9e474da3f256ddbb
Larissa Lira <lrsslira@gmail.com>
2016-07-15 00:10:59 -0300

    Criação da classe Funções

---
+++ TutoGit/TutoGit/Funcoes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutoGit
+{
+    class Funcoes
+    {
+        public void soma(int num1, int num2)
+        {
+
+        }
+
+
+    }
+
+
+}
