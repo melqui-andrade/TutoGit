ab8c034e989936a051b2c99d9a4631bbfc437564
melqui-andrade <melqui.inbox@gmail.com>
2016-07-14 23:32:14 -0300

    Design da calculadora implementado

--- TutoGit/TutoGit/Form1.Designer.cs
+++ TutoGit/TutoGit/Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxNumero1 = new System.Windows.Forms.TextBox();
+            this.txtBoxNumero2 = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número 2:";
+            // 
+            // txtBoxNumero1
+            // 
+            this.txtBoxNumero1.Location = new System.Drawing.Point(87, 31);
+            this.txtBoxNumero1.Name = "txtBoxNumero1";
+            this.txtBoxNumero1.Size = new System.Drawing.Size(59, 20);
+            this.txtBoxNumero1.TabIndex = 2;
+            // 
+            // txtBoxNumero2
+            // 
+            this.txtBoxNumero2.Location = new System.Drawing.Point(87, 62);
+            this.txtBoxNumero2.Name = "txtBoxNumero2";
+            this.txtBoxNumero2.Size = new System.Drawing.Size(59, 20);
+            this.txtBoxNumero2.TabIndex = 3;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(71, 163);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Resultado:";
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Location = new System.Drawing.Point(89, 103);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(13, 13);
+            this.labelResultado.TabIndex = 6;
+            this.labelResultado.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(209, 261);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtBoxNumero2);
+            this.Controls.Add(this.txtBoxNumero1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxNumero1;
+        private System.Windows.Forms.TextBox txtBoxNumero2;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelResultado;
     }
 }
 
